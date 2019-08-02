c5ac2324353936fc9a33470a72a615a2b8eca1f1
SOI-LAN-TRON <truongquanghieu8198@gmail.com>
2019-08-02 16:46:17 +0700

    Update Room from svr

--- svr/handlers/h-Room.js
+++ svr/handlers/h-Room.js
@@ -1,9 +1,12 @@
 const db = require("../models");
-// const ut = require("../ut");
 
 exports.getAll = async(req, res, next) => {
     try{
-        let rooms = await db.Room.find({user: req.params.user_id}).select("_id number people price bill").exec();
+        // let rooms = await db.Room.find({user: req.params.user_id}).select("_id number people price bill").exec();
+        const user_id = req.params.user_id || req.body.user_id;
+        let list = await db.Room.find({user_id}).populate({
+            path: "price_id",
+        }).exec();
         return res.status(200).json(rooms);
     }catch(err){
         return next(err);
@@ -17,12 +20,9 @@ exports.create = async(req, res, next) => {
         req.body.price = listPrice[0]._id;
         let createdRoom = await db.Room.create(req.body);
 
-        // push the id of created room to found price
         listPrice[0].room.push(createdRoom._id);
         listPrice[0].save();
 
-        // await ut.addRoleToUser(220, req.params.user_id);
-        // await ut.pushIdToCol("User", "room", req.params.user_id, createdRoom._id);
         return res.status(200).json(createdRoom);
     }catch(err){
         return next(err);
@@ -32,7 +32,7 @@ exports.create = async(req, res, next) => {
 exports.remove = async(req, res, next) => {
     try{
         let foundRoom = await db.Room.findById(req.params.room_id);
-        await foundRoom.remove();
+        if(foundRoom) await foundRoom.remove();
         return res.status(200).json(foundRoom);
     }catch(err){
         return next(err);
@@ -42,7 +42,10 @@ exports.remove = async(req, res, next) => {
 exports.update = async(req, res, next) => {
     try{
         let foundRoom = await db.Room.findById(req.params.room_id);
-        foundRoom.number = req.body.number;
+        const {people_id, name} = req.body;
+        foundRoom.name = name;
+        foundRoom.people_id = people_id;
+        
         await foundRoom.save();
         return res.status(200).json(foundRoom);
     } catch(err){
