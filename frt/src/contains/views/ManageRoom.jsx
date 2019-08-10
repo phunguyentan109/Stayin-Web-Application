1a051586e600b07e7a0ddb3d75daef49fa003dd3
phulicon1998 <phulicon815490@gmail.com>
2019-08-10 22:32:59 +0700

    Add toggle form button

--- frt/src/contains/views/ManageRoom.jsx
+++ frt/src/contains/views/ManageRoom.jsx
@@ -1,9 +1,17 @@
-import React from "react";
+import React, {useState} from "react";
 import ManageRoom from "components/views/ManageRoom";
 import withAccess from "hocs/withAccess";
 
 function ManageRoomContain(props) {
-    return <ManageRoom {...props}/>
+    const [formIsOpen, setOpenForm] = useState(false);
+
+    const toggleForm = () => setOpenForm(prev => !prev);
+
+    return <ManageRoom
+        {...props}
+        toggleForm={toggleForm}
+        formIsOpen={formIsOpen}
+    />
 }
 
 export default withAccess(ManageRoomContain);
