ddfc524168eeabfcbe11711e4fe08a158fdbc213
phulicon1998 <phulicon815490@gmail.com>
2019-08-14 17:52:56 +0700

    Create some reusable components

--- frt/src/components/views/ManageRoom.jsx
+++ frt/src/components/views/ManageRoom.jsx
@@ -3,7 +3,6 @@ import PropTypes from "prop-types";
 import AppLayoutContain from "contains/Layout/AppLayout";
 import GridItem from "components/Grid/GridItem.jsx";
 import Card from "components/Card/Card.jsx";
-import CardHeader from "components/Card/CardHeader.jsx";
 import CardBody from "components/Card/CardBody.jsx";
 import RoomTable from "components/Table/RoomTable.jsx";
 import GridContainer from "components/Grid/GridContainer.jsx";
@@ -14,46 +13,20 @@ import ConfirmBar from "components/Bar/ConfirmBar";
 import ASBar from "components/Bar/ASBar";
 import PeopleBox from "components/Box/PeopleBox";
 import EmptyBox from "components/Box/EmptyBox";
+import TableCard from "components/Card/TableCard";
+import TitleBox from "components/Box/TitleBox";
 
-import withStyles from "@material-ui/core/styles/withStyles";
-import styles from "assets/cardHeaderStyle";
-
-const ManageRoom = ({classes, formIsOpen, toggleForm, hdConfirm, hdBill, form, room, rooms, hdChange, hdRemove, hdEdit, table, ...props}) => (
+const ManageRoom = ({formIsOpen, toggleForm, hdConfirm, hdBill, form, room, rooms, hdChange, hdRemove, hdEdit, table, people, assignPeople, ...props}) => (
     <AppLayoutContain {...props}>
         {
             formIsOpen && <GridContainer>
                 <GridItem xs={12} sm={12} md={12}>
-                    <Card plain>
-                        <CardHeader plain color="primary">
-                            <h4 className={classes.cardTitleWhite}>
-                                Create New Room
-                            </h4>
-                            <p className={classes.cardCategoryWhite}>
-                                Here is a subtitle for this table
-                            </p>
-                        </CardHeader>
-                    </Card>
-                </GridItem>
-                <GridItem xs={12} sm={12} md={12}>
-                    <Card customCss="custom-card">
-                        <CardBody>
-                            <GridContainer>
-                                <GridItem xs={12} sm={12} md={12}>
-                                    <ConfirmBar
-                                        cancel={toggleForm}
-                                        confirm={hdConfirm}
-                                    />
-                                </GridItem>
-                            </GridContainer>
-                        </CardBody>
-                    </Card>
+                    <TitleBox {...form.box} />
+                    <ConfirmBar cancel={toggleForm} confirm={hdConfirm}/>
                 </GridItem>
                 <GridItem xs={12} sm={12} md={4}>
                     <Card customCss="custom-card">
-                        <CustomCardHeader
-                            title="New Room Information"
-                            subtitle="Please fill in suitable information for your room"
-                        />
+                        <CustomCardHeader {...form.info}/>
                         <CardBody>
                             <GridContainer>
                                 <GridItem xs={12} sm={12} md={12}>
@@ -112,72 +85,58 @@ const ManageRoom = ({classes, formIsOpen, toggleForm, hdConfirm, hdBill, form, r
                 </GridItem>
                 <GridItem xs={12} sm={12} md={6}>
                     <Card customCss="custom-card">
-                        <CustomCardHeader
-                            title="People Assigned List"
-                            subtitle="This is all the people assigned for this room"
-                        />
+                        <CustomCardHeader {...form.people}/>
                         <CardBody>
                             <GridContainer customCss="people-container">
-                                <GridItem xs={12} sm={6} md={3}>
-                                    <PeopleBox
-                                        remove
-                                        link="https://source.unsplash.com/120x120"
-                                    />
-                                </GridItem>
-                                <GridItem xs={12} sm={6} md={3}>
-                                    <PeopleBox
-                                        remove
-                                        link="https://source.unsplash.com/140x140"
-                                    />
-                                </GridItem>
-                                <GridItem xs={12} sm={6} md={3}>
-                                    <PeopleBox
-                                        remove
-                                        link="https://source.unsplash.com/160x160"
-                                    />
-                                </GridItem>
+                                {
+                                    room.people_id.length > 0
+                                    ? room.people_id.map(p => (
+                                        <GridItem xs={12} sm={6} md={3}>
+                                            <PeopleBox
+                                                link={p.user_id.avatar.link}
+                                                name={p.user_id.viewname}
+                                                job={p.job}
+                                                use={p}
+                                                remove={assignPeople}
+                                            />
+                                        </GridItem>
+                                    ))
+                                    : <GridItem xs={12} sm={12} md={12}>
+                                        <EmptyBox
+                                            height="100%"
+                                            message="There is no people assigned for this room"
+                                        />
+                                    </GridItem>
+                                }
                             </GridContainer>
                         </CardBody>
                     </Card>
                 </GridItem>
                 <GridItem xs={12} sm={12} md={6}>
                     <Card customCss="custom-card">
-                        <CustomCardHeader
-                            title="Waiter List"
-                            subtitle="This is all the people who don't have room"
-                        />
+                        <CustomCardHeader {...form.wait}/>
                         <CardBody>
                             <GridContainer customCss="people-container">
-                                <GridItem xs={12} sm={6} md={3}>
-                                    <PeopleBox
-                                        add
-                                        link="https://source.unsplash.com/100x100"
-                                    />
-                                </GridItem>
-                                <GridItem xs={12} sm={6} md={3}>
-                                    <PeopleBox
-                                        add
-                                        link="https://source.unsplash.com/150x150"
-                                    />
-                                </GridItem>
-                                <GridItem xs={12} sm={6} md={3}>
-                                    <PeopleBox
-                                        add
-                                        link="https://source.unsplash.com/250x250"
-                                    />
-                                </GridItem>
-                                <GridItem xs={12} sm={6} md={3}>
-                                    <PeopleBox
-                                        add
-                                        link="https://source.unsplash.com/200x200"
-                                    />
-                                </GridItem>
-                                <GridItem xs={12} sm={6} md={3}>
-                                    <PeopleBox
-                                        add
-                                        link="https://source.unsplash.com/300x300"
-                                    />
-                                </GridItem>
+                                {
+                                    people.length > 0
+                                    ? people.map((p, i) => (
+                                        <GridItem xs={12} sm={6} md={3} key={i}>
+                                            <PeopleBox
+                                                link={p.user_id.avatar.link}
+                                                name={p.user_id.viewname}
+                                                job={p.job}
+                                                use={p}
+                                                add={assignPeople}
+                                            />
+                                        </GridItem>
+                                    ))
+                                    : <GridItem xs={12} sm={12} md={12}>
+                                        <EmptyBox
+                                            height="100%"
+                                            message="There is no waiter for this room"
+                                        />
+                                    </GridItem>
+                                }
                             </GridContainer>
                         </CardBody>
                     </Card>
@@ -187,36 +146,30 @@ const ManageRoom = ({classes, formIsOpen, toggleForm, hdConfirm, hdBill, form, r
         {
             formIsOpen || <GridContainer>
                 <GridItem xs={12} sm={12} md={12}>
-                    <Card>
-                        <CardHeader color="primary">
-                            <h4 className={classes.cardTitleWhite}>{table.title}</h4>
-                            <p className={classes.cardCategoryWhite}>{table.subtitle}</p>
-                        </CardHeader>
-                        <CardBody>
-                            <ASBar create={toggleForm}/>
-                            {
-                                rooms.length > 0
-                                ? <RoomTable
-                                    tableHeaderColor="primary"
-                                    tableHead={table.header}
-                                    tableData={rooms}
-                                    options={{
-                                        remove: hdRemove,
-                                        edit: hdEdit,
-                                        more: [
-                                            {
-                                                icon: "fas fa-file-invoice-dollar",
-                                                name: "View all bills",
-                                                className: "edit",
-                                                fn: hdBill
-                                            }
-                                        ]
-                                    }}
-                                />
-                                : <EmptyBox message={table.empty}/>
-                            }
-                        </CardBody>
-                    </Card>
+                    <TableCard {...table.room.card}>
+                        <ASBar create={toggleForm}/>
+                        {
+                            rooms.length > 0
+                            ? <RoomTable
+                                tableHeaderColor="primary"
+                                tableHead={table.room.header}
+                                tableData={rooms}
+                                options={{
+                                    remove: hdRemove,
+                                    edit: hdEdit,
+                                    more: [
+                                        {
+                                            icon: "fas fa-file-invoice-dollar",
+                                            name: "View all bills",
+                                            className: "edit",
+                                            fn: hdBill
+                                        }
+                                    ]
+                                }}
+                            />
+                            : <EmptyBox message={table.empty}/>
+                        }
+                    </TableCard>
                 </GridItem>
             </GridContainer>
         }
@@ -227,4 +180,4 @@ ManageRoom.propTypes = {
     classes: PropTypes.object
 };
 
-export default withStyles(styles)(ManageRoom);
+export default ManageRoom;
