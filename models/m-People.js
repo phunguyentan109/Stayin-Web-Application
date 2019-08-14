ff939047fff197b574787caf97391fb2321df418
phulicon1998 <phulicon815490@gmail.com>
2019-08-14 17:50:41 +0700

    Working on manage room

--- models/m-People.js
+++ models/m-People.js
@@ -11,7 +11,7 @@ const peopleSchema = new mongoose.Schema({
         type: mongoose.Schema.Types.ObjectId,
         ref: "Room"
     },
-    jobs: String,
+    job: String,
     birthDate: {
         type: Date,
         default: Date.now
